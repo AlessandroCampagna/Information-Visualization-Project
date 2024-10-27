377cbd9d6e2199bb24e4f6e3d98799f839899b94
José Corte <josesantoscorte@gmail.com>
2024-10-27 14:37:59 +0000

    feat: final slider

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import RangeSlider from 'react-range-slider-input';
 import 'react-range-slider-input/dist/style.css';
 
 function App() {
-  const [sliderValues, setSliderValues] = useState([0, 100]);
+  const [sliderValues, setSliderValues] = useState([0, 4]);
 
   useEffect(() => {
     initCharts(); // Initialize the visualizations
@@ -16,8 +16,8 @@ function App() {
     const startYear = 2014;
     const endYear = 2018;
     const modulatedValues = [
-      startYear + Math.floor(values[0] / 100 * (endYear - startYear)),
-      startYear + Math.floor(values[1] / 100 * (endYear - startYear))
+      startYear + Math.floor(values[0] / 4 * (endYear - startYear)),
+      startYear + Math.floor(values[1] / 4 * (endYear - startYear))
     ];
     setTimeRange(modulatedValues[0], modulatedValues[1]);
   };
@@ -27,7 +27,22 @@ function App() {
       <main className="dashboard">
         <div className='container navbar Navbar' id="navbar">
           <h1>Gun Violence in the United States</h1>
-          <RangeSlider className="small-slider" value={sliderValues} onInput={handleSliderChange} />
+          <div className="slider-container">
+            <div className="slider-legend">
+              <span>2014</span>
+              <span>2015</span>
+              <span>2016</span>
+              <span>2017</span>
+              <span>2018</span>
+            </div>
+            <RangeSlider
+              className="slider"
+              min={0}
+              max={4}
+              defaultValue={[0, 4]}
+              onInput={handleSliderChange}
+            />
+          </div>
         </div>
         <div className="container large LineChart" id="linechart"></div>
         <div className="container small HexbinMap" id="hexbinmap"></div>
